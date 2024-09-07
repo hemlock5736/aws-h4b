35e69bb8f10937d4bbf6e3392b95fa599db107b3
hemlock5736 <137481609+hemlock5736@users.noreply.github.com>
2024-09-07 09:30:32 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import {
+  COLUMNS_TYPE1,
+  COLUMNS_TYPE2,
+  COLUMNS_TYPE3,
+  URL_TYPE1,
+  URL_TYPE2,
+  URL_TYPE3,
+} from "./constants";
+import type { Column } from "./type";
+
+(() => {
+  let columns: Column | undefined;
+
+  if (location.href.match(URL_TYPE1)) {
+    columns = COLUMNS_TYPE1;
+  } else if (location.href.match(URL_TYPE2)) {
+    columns = COLUMNS_TYPE2;
+  } else if (location.href.match(URL_TYPE3)) {
+    columns = COLUMNS_TYPE3;
+  }
+
+  if (!columns) {
+    return;
+  }
+
+  for (const id in columns) {
+    const element = document.querySelector<HTMLInputElement>(`#${id}`);
+    if (element) {
+      element.value = columns[id];
+    }
+  }
+})();
